5130c7367019fc8ae4c59d14243cf7707dbfcd67
nltesown <nlte@magic.fr>
2017-04-21 15:12:49 +0200

    Update data, add gauge

--- src/js/main.js
+++ src/js/main.js
@@ -7,52 +7,35 @@ import route from 'riot-route';
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g; // Set mustache-style interpolate delimiters
 moment.locale("fr", { monthsShort: "jan_fév_mar_avr_mai_juin_juil_aoû_sep_oct_nov_déc".split("_"), weekdaysShort: "Dim_Lun_Mar_Mer_Jeu_Ven_Sam".split("_") });
 
+var offsetX = 400;
+var factor = 1.25;
+
 var template = {
   thumb: _.template([
     "<div class='thumb-cont' data-code='{{ code }}'>",
-      "<div class='thumb' style='background-image:url(img/240x200/{{ id }}.jpg);'></div>",
-      "<div class='thumb-overlay'></div>",
-      "<div class='thumb-text'>{{ date.format('D MMM YYYY') }}</div>",
+      "<div class='thumb' style='background-image:url(img/300/{{ id }}.jpg);'></div>",
+      "<div class='thumb-gauge'><div class='thumb-gauge-level'></div></div>",
+      "<div class='thumb-text'>{{ festYear }}</div>",
     "</div>",
   ].join("")),
   content: _.template([
     "<div class='content'>",
-      "<h1>{{ date.format('D MMM YYYY') }}<br>{{ title }}</h1>",
+      "<h1><em>{{ date.format('D MMM YYYY') }}</em><br>{{ title }}</h1>",
       "<div class='text'>{{ text }}</div>",
       "<% _(media).forEach(function (m) { %>",
         "<div class='media-container {{ m.type }}'>",
           "<% if (m.type === 'img') { %>",
             "<img src='img/{{ m.name }}.jpg' alt='{{ m.title.replace(/(<([^>]+)>)/gi, '') }}' title='{{ m.title.replace(/(<([^>]+)>)/gi, '') }}' data-media='{{ JSON.stringify(m) }}'>",
-          "<% } else if (m.type === 'video-ina' || m.type === 'video-dm') { %>",
+          "<% } else if (m.type === 'video') { %>",
             "<iframe src='{{ m.url }}' width='{{ m.size[0] }}' height='{{ m.size[1] }}' frameborder='0' scrolling='no'></iframe>",
           "<% } %>",
           "<div class='legend'>{{ m.title }}</div>",
         "</div>",
       "<% }); %>",
     "</div>"
-
-
-
-
-    // "<div class='content'>",
-    //   "<h1>{{ date.format('D MMM YYYY') }}<br>{{ title }}</h1>",
-    //   "<div class='text'>{{ text }}</div>",
-    //   "<% _(media).forEach(function (m) {",
-    //   "if (m.type === 'img') { %>",
-    //     "<div class='media-container {{ m.type }}'>",
-    //       "<img src='img/{{ m.name }}.jpg' alt='{{ m.title.replace(/(<([^>]+)>)/gi, '') }}' title='{{ m.title.replace(/(<([^>]+)>)/gi, '') }}' data-media='{{ JSON.stringify(m) }}'>",
-    //       "<div class='legend'>{{ m.title }}</div>",
-    //     "</div>",
-    //   "<% } else if (m.type === 'video-ina') { %>",
-    //     "<div class='media-container {{ m.type }}'>",
-    //       "<iframe src='{{ m.url }}' frameborder='0' scrolling='no'></iframe>",
-    //       "<div class='legend'>{{ m.title }}</div>",
-    //     "</div>",
-    //   "<% }",
-    //   "}); %>",
-    // "</div>"
   ].join(""))
-}
+};
+
 
 $(function () {
   $.getJSON("data/data.json").then(run);
@@ -68,7 +51,8 @@ function run (data) {
   .map(
     item => _(item)
       .assign({
-        date: moment(item.date)
+        date: moment(item.date),
+        festYear: parseInt((item.date).match(/^\d{4}/)[0], 10) // Festival year
       })
       .value()
   )
@@ -76,15 +60,24 @@ function run (data) {
 
   _(data)
   .forEach(item => {
-    $("<div class='thumb-sizer size" +  ([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4][item.note] || 1) + "'></div>")
+    $("<div class='thumb-sizer size4'></div>")
+    // $("<div class='thumb-sizer size" +  ([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4][item.note] || 1) + "'></div>")
     .css({
-      top: ([65, 50, 35][item.y - 1] - _.random(0, 5, true)) + "vh",
-      left: (item.x) + "px",
+      // top: ([65, 50, 35][item.y - 1] - _.random(0, 5, true)) + "vh",
+      top: ([60, 40, 20][item.y - 1] - _.random(0, 5, true)) + "vh",
+      left: (item.x * factor + offsetX) + "px",
     })
     .appendTo($(".content-scroller"))
     .html(template.thumb(item))
     .children(".thumb-cont")
-    .data("item", item)
+    .data("item", item);
+
+    gauge(item).on();
+  });
+
+
+  $(window).scroll(() => {
+    console.log($(".content-scroller").scrollLeft());
   });
 
   scroller = new IScroll(".content-wrapper", {
@@ -95,10 +88,10 @@ function run (data) {
     tap: true
   });
 
-  drop($("svg.title path"), 0, 75, false, "bounceInUp");
-  drop($(".thumb-cont"), 200, 10, true, "bounceInDown");
+  // drop($("svg.title path"), 0, 75, false, "bounceInUp");
+  // drop($(".thumb-cont"), 200, 10, true, "bounceInDown");
 
-  var v = new Viewer({
+  v = new Viewer({
     width: "100vw",
     enableRequestClose: true
   });
@@ -145,7 +138,7 @@ function run (data) {
   route.start(true);
 }
 
-
+/*
 function drop($elems, delay, duration, shuffle, animationType) {
   $elems.hide().addClass("animated");
   window.setTimeout(
@@ -162,4 +155,23 @@ function drop($elems, delay, duration, shuffle, animationType) {
     },
     (delay || 0)
   );
+}
+*/
+
+
+function gauge (item) {
+  var $level = $(".thumb-cont[data-code='" + item.code + "']").find(".thumb-gauge-level");
+  return {
+    on: function () {
+      $(this).off();
+      window.setTimeout(() => {
+        $level.css({
+          bottom: (item.note * 10) + "%"
+        });        
+      }, 200);
+    },
+    off: function () {
+      $level.css({ bottom: 0 });
+    }
+  };
 }
